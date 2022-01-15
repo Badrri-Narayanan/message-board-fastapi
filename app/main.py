ab0e2ebb46b125f1090bcb57613569d28d64ac89
Badrri Narayanan S <badrri.s@gartner.com>
2022-01-15 20:42:43 +0530

    added psyopg2 support for CRUD operations

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from typing import Optional
 from pydantic import BaseModel
 from fastapi import FastAPI, status, HTTPException
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
 
 app = FastAPI()
 
@@ -9,57 +12,74 @@ class Message(BaseModel):
     title: str
     content: str
     id: Optional[int] = None
+    published: Optional[bool] = True
 
+
+while True:
+    try:
+        conn = psycopg2.connect(host='localhost',
+                                database='fastapi-db',
+                                user='badrri',
+                                password='secret123',
+                                cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("DB connection successful")
+        break
+    except Exception as error:
+        print("DB connection unsuccessful")
+        print("Error: ", error)
+        time.sleep(3)
 
 messages = [
     {"id": 1, "title": "demo1", "content": "this is demo message 1"},
     {"id": 2, "title": "demo2", "content": "this is demo message 2"},
 ]
 
+
 @app.get("/messages")
 def get_all_messages():
-    return {"data": messages}
+    cursor.execute("""SELECT * FROM tweets;""")
+    all_messages = cursor.fetchall()
+    return {"data": all_messages}
 
 
 @app.get("/messages/{message_id}")
 def get_message_by_id(message_id: int):
-    if message_id > len(messages) or message_id < 1:
+    cursor.execute("""SELECT * FROM tweets where id = %s""", str(message_id))
+    retrieved_message = cursor.fetchone()
+    if not retrieved_message:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'message with id {message_id} is not found')
-    return {"data": [p for p in messages if p["id"] == message_id]}
+    return {"data": retrieved_message}
 
 
 @app.post("/messages", status_code=status.HTTP_201_CREATED)
 def create_message(message: Message):
-    new_message = message.dict()
-    new_message["id"] = len(messages)+1
-    messages.append(new_message)
+    cursor.execute("""INSERT INTO tweets (title, content, published) VALUES (%s,%s,%s) RETURNING * """,
+                   (message.title, message.content, message.published))
+    new_message = cursor.fetchone()
+    conn.commit()
     return {"data": new_message}
-
-
-def find_index_of_message(id):
-    for i, p in enumerate(messages):
-        if p["id"] == id:
-            return i
-    return -1
 
 
 @app.delete("/messages/{message_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_message_by_id(message_id: int):
-    index = find_index_of_message(message_id)
-    if index == -1:
+    cursor.execute("""DELETE FROM tweets WHERE id = %s RETURNING *""", str(message_id))
+    deleted_message = cursor.fetchone()
+    conn.commit()
+    if not deleted_message:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'message with id {message_id} is not found')
-    messages.pop(index)
     return {"data": f'message with id {message_id} was deleted'}
 
 
 @app.put("/messages/{message_id}")
 def update_message(message_id: int, message_info: Message):
-    index = find_index_of_message(message_id)
-    if index == -1:
+    cursor.execute("""UPDATE tweets SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
+                   (message_info.title, message_info.content, message_info.published, str(message_id)))
+    updated_message = cursor.fetchone()
+    conn.commit()
+    if not updated_message:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f'message with id {message_id} is not found')
-    messages[index]["title"] = message_info.title
-    messages[index]["content"] = message_info.content
-    return {"data": "message update successfully"}
+    return {"data": updated_message}
